764a7cb44061e73fd2540a202ad03a41c5b1bb7b
Rachel Cullen <rachel.cullen1@vodafone.com>
2023-11-06 14:32:37 +0000

    trying to add chirpy chick as level 3.

--- app/level-3.tsx
+++ app/level-3.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, View } from 'react-native'
+import {Image, StyleSheet, Text, View} from 'react-native'
+import React from "react";
+import {useAssets} from "expo-asset";
 
 export default function LevelThreeScreen() {
-  return <View style={styles.container} />
+  const [assets] = useAssets([require('../assets/images/chirpychick1.png')]);
+
+  return (<View style={styles.container}>
+    <Text style={styles.header}>Chirpy Chick</Text>
+    <Image
+        style={styles.image}
+        source={assets[0]}
+    />
+  </View>);
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#91E8FC',
+    backgroundColor: '#ffffff',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: 500,
+    height: 500
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginBottom: 20,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
   },
 })
