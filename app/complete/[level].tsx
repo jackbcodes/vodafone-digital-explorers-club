6464206b6ceb8b49f3a53e7a11ce32d29a509351
Rachel Cullen <rachel.cullen1@vodafone.com>
2023-11-07 10:16:45 +0000

    Removing hard coding of level to 2.

--- app/complete/[level].tsx
+++ app/complete/[level].tsx
@@ -13,7 +13,7 @@ export default function ModalScreen() {
   const setLevel = useLevelStore((state) => state.setLevel)
 
   const handleNextLevel = async () => {
-    setLevel(2)
+    setLevel(params.level + 1)
     router.push('/home')
   }
 
