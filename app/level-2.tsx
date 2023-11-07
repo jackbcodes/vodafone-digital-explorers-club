e2b45d2888519eac979be5a3602223654a644c25
Rachel Cullen <rachel.cullen1@vodafone.com>
2023-11-07 09:32:15 +0000

    Making chirpy chick level 2.

--- app/level-2.tsx
+++ app/level-2.tsx
@@ -1,7 +1,99 @@
-import { StyleSheet, View } from 'react-native'
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import {useAssets} from "expo-asset";
+import React, {useState} from "react";
 
 export default function LevelTwoScreen() {
-  return <View style={styles.container} />
+  const [assets] = useAssets([require('../assets/images/chirpychick1.png'),require('../assets/images/chirpychick2.png'), require('../assets/images/chirpychick3.png'), require('../assets/images/chirpychick4.png'), require('../assets/images/chirpychick5.png'), require('../assets/images/chirpychick6.png')]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const passingScore: number = 2;
+  const questions = [
+    {
+      text: 'The chick should have told her grown up where she was going.',
+      isTrue: true,
+    },
+    { text: 'It was a good idea for her to meet her new friend in real life?', isTrue: false },
+    {
+      text: 'People don\'t always use their real photo and give their real age',
+      isTrue: true,
+    },
+  ]
+  const nextPage = () => {
+    setCurrentPage(currentPage + 2);
+  }
+
+  const previousPage = () => {
+    setCurrentPage(currentPage - 2);
+  }
+
+  const handleAnswer = (answer) => {
+    if (answer === questions[currentQuestionIndex].isTrue) {
+      setScore(score + 1);
+    }
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex < questions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      const passed = score >= passingScore;
+      alert(`Game Over! Your score is ${score} ${passed ? 'You passed!' : 'Try again!'}`);
+      setCurrentQuestionIndex(0);
+      setScore(0);
+    }
+  };
+
+  function showImagesOrQuestions() {
+    if (currentPage > 4) {
+      console.log('we should be showing a question here');
+      return (<View style={styles.container}>
+        <Text style={styles.question}>{questions[currentQuestionIndex].text}</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleAnswer(true)}
+          >
+            <Text style={styles.buttonText}>True</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleAnswer(false)}
+          >
+            <Text style={styles.buttonText}>False</Text>
+          </TouchableOpacity>
+        </View>
+      </View>);
+    } else {
+      return (<View style={styles.container}>
+        <Image
+            style={styles.image}
+            source={assets[currentPage]}
+        />
+        <Image
+            style={styles.image}
+            source={assets[currentPage + 1]}
+        />
+      </View>);
+    }
+  }
+
+  return (<View style={styles.container}>
+    <Text style={styles.header}>Chirpy Chick</Text>
+    {showImagesOrQuestions()}
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+          style={styles.button}
+          onPress={() => previousPage()}
+      >
+        <Text style={styles.buttonText}>Previous Page</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+          style={styles.button}
+          onPress={() => nextPage()}
+      >
+        <Text style={styles.buttonText}>Next Page</Text>
+      </TouchableOpacity>
+    </View>
+  </View>);
 }
 
 const styles = StyleSheet.create({
@@ -9,6 +101,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#91E8FC',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  header: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  question: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
   },
 })
--- app/level-3.tsx
+++ app/level-3.tsx
@@ -1,38 +1,14 @@
-import {Image, StyleSheet, Text, View} from 'react-native'
-import React from "react";
-import {useAssets} from "expo-asset";
+import { StyleSheet, View } from 'react-native'
 
 export default function LevelThreeScreen() {
-  const [assets] = useAssets([require('../assets/images/chirpychick1.png')]);
-
-  return (<View style={styles.container}>
-    <Text style={styles.header}>Chirpy Chick</Text>
-    <Image
-        style={styles.image}
-        source={assets[0]}
-    />
-  </View>);
+  return <View style={styles.container} />
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#91E8FC',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    width: 500,
-    height: 500
-  },
-  image: {
-    width: 300,
-    height: 300,
-    marginBottom: 20,
-  },
-  header: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 20,
   },
 })
